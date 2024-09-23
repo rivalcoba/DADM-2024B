6a6ac05b9f5938c13cef95034353e2fe27f8bac0
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2024-09-23 18:53:33 +0000

    ⭐ FEAT: App Create lists of items in model

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,22 @@
   import { ref } from 'vue';
   // Modelo
   const header = ref('App lista de compras');
+  const items = ref([
+    {id:'0', label: '10 bolillos'},
+    {id:'1', label:'1 lata frijoles'},
+    {id:'2', label:'1 Chelas'},
+    {id:'3', label:'1 Nutella'},
+  ]);
 </script>
 
 <template>
   <h1>
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
-    </h1>
+  </h1>
+  <ul>
+    <li v-for="item in items" :key="item.id"> 🛍️ {{ item.label }} </li>
+  </ul>
 </template>
 
 <style scoped>
