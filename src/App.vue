78d65830a983a61849a279048e4b345eee18ebbc
rivalcoba <ivan.rivalcoba@gmail.com>
2024-10-02 12:36:09 -0500

    ⭐ S11 Methods Create method to add item and clean input

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,33 @@
   import { ref } from 'vue';
   // Modelo
   const header = ref('App lista de compras');
+  // --- items ---
+  // Item-Model
   const items = ref([
     {id:'0', label: '10 bolillos'},
     {id:'1', label:'1 lata frijoles'},
     {id:'2', label:'1 Chelas'},
     {id:'3', label:'1 Nutella'},
   ]);
+  // Item-Method
+  const saveItem = () => {
+    // Add new item
+    items.value.push({id: items.value.length + 1, label: newItem.value});
+    // Clean the input
+    newItem.value = '';
+  };
+
   const newItem = ref('');
   const newItemHighPriority = ref(false);
+
 </script>
 
 <template>
   <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
   <!-- Agrupando Entradas de usuario -->
-  <form class="add-item form" v-on:submit.prevent="items.push({id: items.length + 1, label: newItem})">
+  <form 
+    class="add-item form" 
+    v-on:submit.prevent="saveItem">
     <!-- Entrada de texto -->
     <input 
       type="text" 
