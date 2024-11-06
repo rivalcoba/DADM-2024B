c15fb01b757c51c5d5429ecdf72ff9801021642e
rivalcoba <ivan.rivalcoba@gmail.com>
2024-11-06 10:49:05 -0600

    ⭐ S02S02 Coffee-plans Add plan-item components

---
+++ src/components/plan-item.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="plan">
+    <div class="description">
+      <span class="title"> El adicto </span>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
