16fb638c5f4238e60eb3f1d94303c7d230aa6b7e
rivalcoba <ivan.rivalcoba@gmail.com>
2024-11-08 10:52:59 -0600

    ⭐ S03S04 Componente PlanPicker

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup>
-import { ref } from 'vue';
-import planItem from './components/plan-item.vue';
-const plans = 
-  ref(['El cafetero', 'El chelero', 'El intelectual']);
+import PlanPicker from './components/PlanPicker.vue';
 </script>
 
 <template>
@@ -20,9 +17,8 @@ const plans =
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
 
-    <div class="plans">
-      <plan-item v-for="plan in plans" :key="plan" :name="plan" />
-    </div>
+    <PlanPicker />
+    
   </div>
 </template>
 
---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import PlanItem from './PlanItem.vue';
+  import { ref } from 'vue';
+const plans = 
+  ref(['El cafetero', 'El chelero', 'El intelectual']);
+</script>
+
+<template>
+<div class="plans">
+  <PlanItem v-for="plan in plans" :key="plan" :name="plan" />
+</div>
+</template>
+
+<style scoped>
+
+</style>
